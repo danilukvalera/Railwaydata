d89a5c24561b712cb6146926b0ae548adb9405e9
valerii <danilukvalera@ukr.net>
2023-07-08 11:29:26 +0300

    Начало проекта

---
+++ src/test/java/com/danyliuk/testjob/railwaydata/RailwaydataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danyliuk.testjob.railwaydata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwaydataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

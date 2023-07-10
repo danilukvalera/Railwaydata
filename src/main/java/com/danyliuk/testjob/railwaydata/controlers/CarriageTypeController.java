b576375ef5118599bd3c369fb7e5d53fdd7952b3
valerii <danilukvalera@ukr.net>
2023-07-10 12:26:23 +0300

    успешное подключение к СУБД mariaDB

--- src/main/java/com/danyliuk/testjob/railwaydata/controlers/CarriageTypeController.java
+++ src/main/java/com/danyliuk/testjob/railwaydata/controlers/CarriageTypeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller // This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
 public class CarriageTypeController {
@@ -26,7 +27,8 @@ public class CarriageTypeController {
         CarriageType carriageType = new CarriageType();
         carriageType.setType(type);
         carriageType.setNumberSeats(numberSeats);
-        carriageTypeRepository.save(carriageType);
+        carriageType = carriageTypeRepository.save(carriageType);
+//        Iterable<CarriageType> ccc = carriageTypeRepository.findAll();
         return "Saved";
     }
 
